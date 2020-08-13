2fb7e2170377449e77bbbdf58951b7f01f844204
pedrovrima <pedrovrima@gmail.com>
2020-08-13 17:46:11 -0300

    Updates

---
+++ precache-manifest.db212989c6fd98592f86e112b4c90741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f07a6ab94d1624aad5489935c67f4e5",
+    "url": "/splats-prototype/index.html"
+  },
+  {
+    "revision": "8fabe76d017b38bc291d",
+    "url": "/splats-prototype/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901ee4709d55bca8307c",
+    "url": "/splats-prototype/static/js/2.4e1f7302.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/splats-prototype/static/js/2.4e1f7302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fabe76d017b38bc291d",
+    "url": "/splats-prototype/static/js/main.c6d56ad5.chunk.js"
+  },
+  {
+    "revision": "3cf34b869847b055294c",
+    "url": "/splats-prototype/static/js/runtime-main.dc140329.js"
+  }
+]);
