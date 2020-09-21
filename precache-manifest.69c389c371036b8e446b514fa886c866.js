6a2090b8413e986d29505e51b09bb9745e9e3d1f
pedrovrima <pedrovrima@gmail.com>
2020-09-21 16:51:55 -0300

    Updates

---
+++ precache-manifest.69c389c371036b8e446b514fa886c866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe38572544d8ddd0983bcdf6c69b3d2b",
+    "url": "/splats-prototype/index.html"
+  },
+  {
+    "revision": "8b1e8a876270b8480335",
+    "url": "/splats-prototype/static/css/main.51f5cdac.chunk.css"
+  },
+  {
+    "revision": "09160546b1e1aeafdeaa",
+    "url": "/splats-prototype/static/js/2.e9808e2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/splats-prototype/static/js/2.e9808e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1e8a876270b8480335",
+    "url": "/splats-prototype/static/js/main.651f3360.chunk.js"
+  },
+  {
+    "revision": "3cf34b869847b055294c",
+    "url": "/splats-prototype/static/js/runtime-main.dc140329.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/splats-prototype/precache-manifest.b2a3f7e2aaedde1215e01a8305a456de.js"
+  "/splats-prototype/precache-manifest.69c389c371036b8e446b514fa886c866.js"
 );
 
 self.addEventListener('message', (event) => {
